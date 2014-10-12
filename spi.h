baed116fa9259deb5a59fc8951f6b71719c8e0d1
Saptarshi Hazra <saptarshiv2hazra@gmail.com>
2014-10-12 23:20:40 +0530

    Initial commit

---
+++ spi.h
@@ -0,0 +1,19 @@
+/*
+ * Multi Purpose SPI Header Library
+ * 
+ * List of Compatible Devices
+ * =================================
+ * * Atmega8
+ *
+ * Created by Saptarishi Hazra and Shoumik Ghosal
+ */ 
+
+#ifndef _SPIPORTS_H_
+/* No port definition has been included before
+ * Assuming default port definitions for Atmega8
+ */
+
+#include "spiports-atmega8.h"
+#endif /* _SPIPORTS_H_ */
+
+#define _SPI_H_
---
+++ spiports-atmega8.h
@@ -0,0 +1,38 @@
+/*
+ * SPI Port Definitions for device - Atmega8
+ * =========================================
+ *
+ * Created by Saptarishi Hazra and Shoumik Ghosal
+ *
+ * Pin mappings are as follows:
+ * PORTB - Port for SPI
+ * DDRB - Data Direction Register for SPI
+ * PB2 - Slave Select (SS)
+ * PB3 - Master Out, Slave In (MOSI)
+ * PB4 - Master In, Slave Out (MISO)
+ * PB5 - Serial Clock (SCK)
+ *
+ */ 
+
+
+#ifndef _SPIPORTS_H_
+#define _SPIPORTS_H_
+
+/* Do initial definitions of ports here
+ * @param SPIPORT - The variable used to mask the SPI port, per device
+ * @param SPIDDR - The variable used the mask the SPI Data Direction Register, per device
+ * @param SS - The variable used to mask the per-device slave select
+ * @param MOSI - The variable used to mask the per-device master-out, slave in
+ * @param MISO - The variable used to mask the per-device master-in, slave out
+ * @param SCK - The variable used to mask the per-device serial clock
+ */
+
+#define SPIPORT PORTB
+#define SPIDDR DDRB
+
+#define SS PB2
+#define MOSI PB3
+#define MISO PB4
+#define SCK PB5
+
+#endif /* _SPIPORTS_H_ */
