39024b6e574e521170b7842a65b6543a4bf779de
Saptarshi Hazra <saptarshiv2hazra@gmail.com>
2014-12-18 12:28:14 +0530

    Added modular interrupt code

---
+++ interruptpins-atmega8.h
@@ -0,0 +1,16 @@
+/*
+ * IncFile1.h
+ *
+ * Created: 12/18/2014 12:26:17 PM
+ *  Author: saptarshi
+ */ 
+
+
+#ifndef INTERRUPT_SETUP_H_
+#define INTERRUPT_SETUP_H_
+
+
+
+
+
+#endif /* INTERRUPT_SETUP_H_ */
